bd98bbc1d96ff61203748c88d21239df567bc8e6
haroon <haroonshahzad203@gmail.com>
2025-04-25 19:38:48 +0500

    console added for env variables

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -7,12 +7,16 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
 
+const { SHOPIFY_API_KEY,SHOPIFY_API_SECRET,SCOPES, SHOPIFY_APP_URL   } = process.env
+
+console.log({SHOPIFY_API_KEY,SHOPIFY_API_SECRET,SCOPES, SHOPIFY_APP_URL})
+
 const shopify = shopifyApp({
-  apiKey: process.env.SHOPIFY_API_KEY,
-  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
+  apiKey: SHOPIFY_API_KEY,
+  apiSecretKey: SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.January25,
-  scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  scopes: SCOPES?.split(","),
+  appUrl: SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
